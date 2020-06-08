9f621fb8aef2c05e6648cc4af1c6f62f03d0f6e2
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-08 10:47:48 -0700

    troubleshoot console log input values

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-    const color = $("input#color").val();
-    const band = $("input#band").val();
-    const movie = $("input#movie").val();
-    const flavor = $("input#flavor").val();
-    const planet = $("input#planet").val();
+    let color = $("input#color").val();
+    let band = $("input#band").val();
+    let movie = $("input#movie").val();
+    let flavor = $("input#flavor").val();
+    let planet = $("input#planet").val();
 
     console.log(color);
     console.log(band);
     console.log(movie);
     console.log(flavor);
     console.log(planet);
+
+    $(".color").text(color);
+    $(".band").text(band);
+    
 
 
     $('#output').show();
