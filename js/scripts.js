14735863700cbe85e8e572e5476da24b7150cc84
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-08 11:36:16 -0700

    this version is not working

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,22 @@ $(document).ready(function() {
     let flavor = $("input#flavor").val();
     let planet = $("input#planet").val();
 
+    // I am missing the value in the html form so there is no console.log output
+
     console.log(color);
     console.log(band);
     console.log(movie);
     console.log(flavor);
     console.log(planet);
 
+
     $(".color").text(color);
     $(".band").text(band);
-    
+    $(".movie").text(movie);
+    $(".flavor").text(flavor);
+    $(".planet").text(planet);
+
+   
 
 
     $('#output').show();
